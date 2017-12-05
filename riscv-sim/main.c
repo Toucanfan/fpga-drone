c7e445a9eb5079686b74570d848068799174e5fe
Troels Mæhl Folke <troelsmf@gmail.com>
2017-12-05 13:47:19 +0100

    riscv-sim: Implemented OP (alu instructions)

--- riscv-sim/main.c
+++ riscv-sim/main.c
@@ -1,4 +1,5 @@
 #include <fcntl.h>
+#include <stdarg.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -100,9 +101,16 @@ static uint32_t get_j_imm(const uint32_t instr) {
 	return (imm_20 << 20)|(imm_19_12 << 19)|(imm_11 << 11)|(imm_10_1 << 10);
 }
 
+static void verbose_printf(const char *format, ...) {
+	va_list ap;
+	va_start(ap, format);
+
+	vprintf(format, ap);
+}
+
 
 static void trap_invalid_instr(void) {
-	printf("invalid ");
+	verbose_printf("invalid ");
 }
 
 static void exec_op_load(uint32_t instr) {
@@ -113,32 +121,31 @@ static void exec_op_load(uint32_t instr) {
 	switch(get_funct3(instr)) {
 	case 0: /* 000 LB */
 		M.regs[rd] = sign_extend((uint32_t)(*((uint8_t *)&M.mem[eff])), 7);
-		printf("lb x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
+		verbose_printf("lb x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
 		break;
 	
 	case 1: /* 001 LH */
 		M.regs[rd] = sign_extend((uint32_t)(*((uint16_t *)&M.mem[eff])), 15);
-		printf("lh x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
+		verbose_printf("lh x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
 		break;
 
 	case 2: /* 010 LW */
 		M.regs[rd] = *((uint32_t *)&M.mem[eff]);
-		printf("lw x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
+		verbose_printf("lw x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
 		break;
 
 	case 4: /* 100 LBU */
 		M.regs[rd] = (uint32_t)(*((uint8_t *)&M.mem[eff]));
-		printf("lbu x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
+		verbose_printf("lbu x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
 		break;
 
 	case 5: /* 101 LHU */
 		M.regs[rd] = (uint32_t)(*((uint16_t *)&M.mem[eff]));
-		printf("lhu x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
+		verbose_printf("lhu x%d,%d(x%d) ", rd, (int32_t)offset, rs1);
 		break;
 		
 	default:
 		trap_invalid_instr();
-		return;
 		break;
 	}
 
@@ -146,55 +153,128 @@ static void exec_op_load(uint32_t instr) {
 }
 
 static void exec_op_imm(uint32_t instr) {
-	printf("OP_IMM ");
+	uint32_t rd = get_rd(instr);
+	uint32_t rs1 = get_rs1(instr);
+	uint32_t shamt = get_rs2(instr);
+	uint32_t funct7 = get_funct7(instr);
+	uint32_t imm = get_i_imm(instr);
+
+	switch(get_funct3(instr)) {
+	case 0: /* 000 ADDI */
+		imm = sign_extend(imm, 11);
+		M.regs[rd] = M.regs[rs1] + imm;
+		verbose_printf("addi x%d,x%d,%d ", rd, rs1, imm);
+		break;
+	case 1: /* 001 */
+		switch(funct7) {
+		case 0x00: /* 000 0000 SLLI */
+			M.regs[rd] = M.regs[rs1] << shamt;
+			verbose_printf("slli x%d,x%d,%d ", rd, rs1, shamt);
+			break;
+		default:
+			trap_invalid_instr();
+			break;
+		}
+		break;
+	case 2: /* 010 SLTI */
+		imm = sign_extend(imm, 11);
+		if ((int32_t)M.regs[rs1] < (int32_t)imm)
+			M.regs[rd] = 1;
+		else
+			M.regs[rd] = 0;
+		verbose_printf("slti x%d,x%d,%d ", rd, rs1, imm);
+		break;
+	case 3: /* 011 SLTIU */
+		imm = sign_extend(imm, 11);
+		if (M.regs[rs1] < imm)
+			M.regs[rd] = 1;
+		else
+			M.regs[rd] = 0;
+		verbose_printf("sltiu x%d,x%d,%d ", rd, rs1, imm);
+		break;
+	case 4: /* 100 XORI */
+		imm = sign_extend(imm, 11);
+		M.regs[rd] = M.regs[rs1] ^ imm;
+		verbose_printf("xori x%d,x%d,%d ", rd, rs1, imm);
+		break;
+	case 5: /* 101 */
+		switch(funct7) {
+		case 0x00: /* 000 0000 SRLI */
+			M.regs[rd] = M.regs[rs1] >> shamt;
+			verbose_printf("srli x%d,x%d,%d ", rd, rs1, shamt);
+			break;
+		case 0x20: /* 010 0000 SRAI */
+			M.regs[rd] = (uint32_t)(((int32_t)M.regs[rs1]) >> shamt);
+			verbose_printf("srai x%d,x%d,%d ", rd, rs1, shamt);
+			break;
+		default:
+			trap_invalid_instr();
+			break;
+		}
+		break;
+	case 6: /* 110 ORI */
+		imm = sign_extend(imm, 11);
+		M.regs[rd] = M.regs[rs1] | imm;
+		verbose_printf("ori x%d,x%d,%d ", rd, rs1, imm);
+		break;
+	case 7: /* 111 ANDI */
+		imm = sign_extend(imm, 11);
+		M.regs[rd] = M.regs[rs1] & imm;
+		verbose_printf("andi x%d,x%d,%d ", rd, rs1, imm);
+		break;
+	default:
+		trap_invalid_instr();
+		break;
+	}
+	/* verbose_printf("OP_IMM "); */
 }
 
 static void exec_op_auipc(uint32_t instr) {
-	printf("OP_AUIPC ");
+	verbose_printf("OP_AUIPC ");
 }
 
 static void exec_op_store(uint32_t instr) {
-	printf("OP_STORE ");
+	verbose_printf("OP_STORE ");
 }
 
 static void exec_op(uint32_t instr) {
-	printf("OP ");
+	verbose_printf("OP ");
 }
 
 static void exec_op_lui(uint32_t instr) {
-	printf("OP_LUI ");
+	verbose_printf("OP_LUI ");
 }
 
 static void exec_op_branch(uint32_t instr) {
-	printf("OP_BRANCH ");
+	verbose_printf("OP_BRANCH ");
 }
 
 static void exec_op_jalr(uint32_t instr) {
-	printf("OP_JALR ");
+	verbose_printf("OP_JALR ");
 }
 
 static void exec_op_jal(uint32_t instr) {
-	printf("OP_JAL ");
+	verbose_printf("OP_JAL ");
 }
 
 static void exec_op_system(uint32_t instr) {
-	printf("OP_SYSTEM ");
+	verbose_printf("OP_SYSTEM ");
 }
 
 static void run_machine_cycle(void) {
 	/* Fetch */
 	uint32_t instr = *((uint32_t *)&M.mem[M.pc]);
-	printf("%.8x: %.8x - ", M.pc, instr);
+	verbose_printf("%.8x: %.8x - ", M.pc, instr);
 	
 	/* Decode */
 
 	/* check that instr is 32-bit length */
 	if ((instr & 0x03) != 0x03) {
-		printf("invalid\n");
+		verbose_printf("invalid\n");
 		fprintf(stderr, "Not 32-bit instruction!\n");
 		exit(EXIT_FAILURE);
 	} else if ((instr & 0x1C) == 0x1C) { /* 1 1100 */
-		printf("invalid\n");
+		verbose_printf("invalid\n");
 		fprintf(stderr, "Not 32-bit instruction (2)!\n");
 		exit(EXIT_FAILURE);
 	}
@@ -236,7 +316,7 @@ static void run_machine_cycle(void) {
 		break;
 	}
 
-	printf("\n");
+	verbose_printf("\n");
 
 	M.pc += 4;
 }
