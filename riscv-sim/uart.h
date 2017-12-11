b0dced02264858ab26aeaa8c2dbd1aaed1cd474e
Troels Mæhl Folke <troelsmf@gmail.com>
2017-12-11 01:45:32 +0100

    riscv-sim: added skeleton uart implementation

---
+++ riscv-sim/uart.h
@@ -0,0 +1,14 @@
+#ifndef RVSIM_UART_H
+#define RVSIM_UART_H 1
+
+#include <stdint.h>
+
+enum {
+	UART_CSR,
+	UART_DATA,
+};
+
+extern uint32_t uart_get_register(uint32_t reg);
+extern void uart_set_register(uint32_t reg, uint32_t value);
+
+#endif
